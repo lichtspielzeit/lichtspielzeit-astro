b67464b3f5ad13dba8e2e3608d0051fc4d877a40
Virginia Kuhlen <virginia.kuhlen@gmx.de>
2025-07-13 21:51:53 +0200

    refactor: Move animation definition to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			// HIER DEFINIEREN WIR DIE ANIMATION FÜR TAILWIND
+			keyframes: {
+				'slow-zoom': {
+					'0%': { transform: 'scale(1)' },
+					'100%': { transform: 'scale(1.1)' },
+				}
+			},
+			animation: {
+				'slow-zoom': 'slow-zoom 25s ease-in-out infinite alternate',
+			}
+		},
 	},
 	plugins: [],
 }
