c4e4ba9fc2e002a7052a6ffc2d702cd4c8b62059
Virginia Kuhlen <virginia.kuhlen@gmx.de>
2025-07-13 21:56:41 +0200

    chore: Revert animation attempts to stable base

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			// HIER DEFINIEREN WIR DIE ANIMATION FÜR TAILWIND
-			keyframes: {
-				'slow-zoom': {
-					'0%': { transform: 'scale(1)' },
-					'100%': { transform: 'scale(1.1)' },
-				}
-			},
-			animation: {
-				'slow-zoom': 'slow-zoom 25s ease-in-out infinite alternate',
-			}
-		},
-	},
-	plugins: [],
-}
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(  )]
+});
